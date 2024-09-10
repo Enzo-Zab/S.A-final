c3c5c5041398e0b9d0c7e7e9e65fb7ce5a023df4
Enzo Zabadal <125047212+Enzo-Zab@users.noreply.github.com>
2024-09-10 15:13:49 -0300

    final

--- src/páginas/sobre.jsx
+++ src/páginas/sobre.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import Logo from '../img/logo.png'; // Assuming this path is correct
-import '../App.css';
+import Img2 from '../img/w.jpeg';
+import '../App.css'; // img src={Logo} alt="Logo" style={{ width: '550px', height: 'auto' }}
 
 function Sobre() {
     return (
       <>
       <div className="aling">
-        <img src={Logo} alt="Logo" style={{ width: '550px', height: 'auto' }} />
+        <h1>Nós estamos ansiosos para te ter aqui!</h1>
+        <img src={Img2} alt="Logo" style={{ width: '700px', height: 'auto' }}/>
+        
         <h1>Sobre</h1>
         <p>
           A pousada Quinta do Ypuã oferece ao seus clientes um recanto de aconchego e lazer, em ambiente rústico e agradável. Ideal para quem gosta de fugir da rotina e procura um local de paz para descansar e curtir a natureza.
           "O Ypuã tem tudo a ver com a natureza, dá para sentir a energia do lugar. Eu me preocupo se você vai comer bem, dormir bem e se vai se sentir em casa. Vou te mostrar onde encontrar os melhores frutos do mar, onde curtir a melhor praia e as melhores ondas. Mas se você não quiser fazer nada eu também conheço o melhor lugar."
-          — Heitor, Anfitrião da pousada
+          <br /><br />— Heitor, Anfitrião da pousada
         </p>
       </div>
       </>
